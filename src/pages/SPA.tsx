f4a4c0e5d678f58105794a08e2e9b596edeffc32
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-05-01 17:14:01 +0530

    sections added in page

--- src/pages/SPA.tsx
+++ src/pages/SPA.tsx
@@ -1,23 +1,37 @@
 'use client'
 
+import { AboutSection } from '../components/AboutSection'
 import { Navbar } from '../components/Navbar'
-import ParticleCanvas from './ParticleCanvas'
+import ProjectsSection from '../components/ProjectsSection'
+import RetroCharacter from '../components/RetroCharacter'
 
 const SPA = () => {
     return (
         <>
-            {/* 1) draw your canvas first */}
-            <section id="home" className="relative w-full h-screen overflow-hidden">
-                <ParticleCanvas />
-                {/* optional hero content */}
-            </section>
 
-            {/* 2) then render the navbar (and rest of page) */}
-            <Navbar />
+
 
             <main className="flex flex-col">
-                <section id="about" className="min-h-screen">{/*...*/}</section>
-                {/* etc. */}
+                <section id="home" className="min-h-screen font-16bit">
+                    <div className="w-full max-w-screen-xl mx-auto ">
+                        <RetroCharacter />
+                    </div>
+                </section>
+                <Navbar />
+
+                <section id="about" className="min-h-screen  ">
+                    <div className="w-full max-w-screen-xl mx-auto ">
+                        <AboutSection/>
+                    </div>
+                </section>
+
+                <section id="projects" className="  ">
+                    <div className="w-full max-w-screen-xl mx-auto ">
+                        <ProjectsSection/>
+                    </div>
+                </section>
+
+                {/* Add similar wrappers to other sections */}
             </main>
         </>
     )
